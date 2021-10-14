2841b18e5480e9163d4b3e9c8cfe54ed8143a6bf
Vinicius Jara <viniciusfjara@gmail.com>
2021-10-14 11:53:55 -0400

    adicionada ordenação dos cursos por data e por curso realizado

--- page-cursos.php
+++ page-cursos.php
@@ -20,19 +20,28 @@
   <div class="row mb-5 mt-1">
 
     <!-- ABERTURA DE REPETIDOR MODELO CMB2 PARA CARDS DE CURSO-->
-      <?php $cursos = get_field('curso'); ?>
-      <?php if(isset($cursos)) {foreach($cursos as $curso) { ?>
+      <?php $cursos = get_field('curso');
+
+
+      usort($cursos, function($a1, $a2) {
+        $date_v1 = str_replace('/', '-', $a1['data_curso']);
+        $date_v2 = str_replace('/', '-', $a2['data_curso']);
+        $v1 = strtotime($date_v1);
+        $v2 = strtotime($date_v2);
+        //return $v1 - $v2;
+        return $v2 - $v1; //reverse direction
+        });   
+      ?>
+
+      <?php if(isset($cursos)) {foreach($cursos as $curso) { 
+        if($curso['curso_realizado']== false) {
+        ?>
       
 
 
         <!-- MOLDE DE EXIBIÇÃO DE CURSO -->
 
       <div id="coluna-curso" class="col-md-4 mb-3 position-relative" >
-
-        <!-- MARCAÇÃO DE CURSO JÁ REALIZADO -->
-        <?php if ($curso['curso_realizado'] == true ) : ?>
-          <div class="realizado-badge">CURSO REALIZADO</div>
-        <?php endif; ?>
 
         <div id="card-curso" class="cursos position-relative" style="background-image: url(<?php echo $curso['bg_curso'];?>)">
           <div class="local-badge position-absolute top-0 start-100"><?php echo $curso['local_curso']; ?></div>
@@ -50,11 +59,47 @@
         </div>
       </div>
 <!-- FIM DO MOLDE-->
-    <?php $curso['curso-realizado'] = 0; ?>
 
 
-    <?php } } ?>         
+
+    <?php } } }?>         
 <!-- FIM DA REPETIÇÃO -->
+
+<?php if(isset($cursos)) {foreach($cursos as $curso) { 
+        if($curso['curso_realizado']== true) {
+        ?>
+      
+
+
+        <!-- MOLDE DE EXIBIÇÃO DE CURSO -->
+
+      <div id="coluna-curso" class="col-md-4 mb-3 position-relative" >
+
+        <!-- MARCAÇÃO DE CURSO JÁ REALIZADO -->
+
+          <div class="realizado-badge">CURSO REALIZADO</div>
+
+
+        <div id="card-curso" class="cursos position-relative" style="background-image: url(<?php echo $curso['bg_curso'];?>)">
+          <div class="local-badge position-absolute top-0 start-100"><?php echo $curso['local_curso']; ?></div>
+            <h3 class="titulo-cursos"><?php echo $curso['titulo_curso']; ?>
+              <hr>
+            </h3>
+            <div class="dados-curso">
+              <ul class="texto-cursos list-unstyled">
+                <li><strong>Local: </strong><span class="badge rounded-pill bg-warning text-dark"><?php echo $curso['local_curso']; ?></span></li>
+                <li><strong>Data: </strong> <?php echo $curso['data_curso']; ?></li>
+                <li><strong>Investimento: </strong>a partir de R$ <?php echo $curso['preco_curso']; ?></li>
+                <hr class="mb-5">
+            </div>
+          <a href="<?php echo $curso['link_curso']; ?>" target="_blank" class="btn btn-1 btn-position">Saiba mais</a>
+        </div>
+      </div>
+<!-- FIM DO MOLDE-->
+
+
+
+    <?php } } }?>         
       
   </div>
 </div>
